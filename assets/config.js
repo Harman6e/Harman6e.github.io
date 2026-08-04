499b43033ca1f4b72fdae0ea330ddd583a7ef4f7
Harmanjot Singh <kalsiharman08@gmail.com>
2026-08-04 12:41:55 +0300

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+window.TORNDEALS_CONFIG = {
+  supabaseUrl: "https://zzywhnvevnoattbamfse.supabase.co",
+  supabaseKey: "sb_publishable_n707H1yGUvAJ0wElEBRhPw_K_8S2Evw",
+  siteUrl: "https://torndeals.me"
+};
